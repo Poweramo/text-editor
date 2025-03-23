59dd231cd634a126f6058d9ac2c4b5211d7f2bb6
Poweramo <assirouomar@gmail.com>
2025-03-23 18:01:33 +0000

    feat: keep textArea line breaks on submit
    using pre element

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+const textArea = document.getElementById("text-container")
 
+document.querySelector("form").addEventListener("submit", () => {
+	textArea.value = "<pre>" + textArea.value + "</pre>"
+})
